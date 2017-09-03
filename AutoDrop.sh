cb35793115553c13676b6a569a0d062457f03bfb
mmiscool <mike@smbisoft.com>
2017-09-03 14:58:02 -0400

    Making System more modular. Adde prusa support and some other stuff.

--- AutoDrop.sh
+++ AutoDrop.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 # include this boilerplate
 
-#These are all the settings for the printer. Change these 
-
-printerServer='http://AUTODROP3D.COM/printerinterface/gcode'
-printerName='JOHNSUCKS'
-printerColor='RED'
-printerMaterial='PLA'
-SIZEX='50'
-SIZEY='50'
-SIZEZ='300'
-
+#These are all the settings for the printer. Change these
+#date +'Script initialized at %Y-%m-%d-%H%M' >> /home/pi/3d.log
+cd /autodrop
+source settings.sh
 
 function jumpto
 {
@@ -25,56 +19,24 @@ TheTop:
 clear
 figlet AutoDrop3d.com
 rm -f download.gcode
-wget -O download.gcode "$printerServer?name=$printerName&Color=$printerColor&material=$printerMaterial&SizeX=$SIZEX&SizeY=$SIZEY&SizeZ=$SIZEZ"
-rm -f printpage.txt
-sed -n '2,10p' download.gcode >> printpage.txt
+wget -O download.gcode "$printerServer?name=$printerName&material=$printerMaterial&SizeX=$SIZEX&SizeY=$SIZEY&SizeZ=$SIZEZ"
 
-echo $printerName >> printpage.txt
-echo "Printed on $(date)" >> printpage.txt
-
+#Check for the start instruction
 read -r PrintQueueInstruction<download.gcode
 
 if test "$PrintQueueInstruction" == ";start" ; then
 	clear
 	figlet Starting Print
+	PrintJobID=`sed -n '3p' download.gcode`
 	jumpto PrintThePart
 fi
 
-echo $PrintQueueInstruction
+figlet $PrintQueueInstruction
 sleep 10
 jumpto TheTop
 exit
 
 
 PrintThePart:
-PrintJobID=`sed -n '3p' download.gcode`
-
-
-./printcore.py -b 76800 -v '/dev/ttyS0' start1.gcode
-sleep 10
-#this is to print the file
-lpr printpage.txt
-
-
-CheckPrintStaus:
-figlet Wating For Printer
-
-PrintStatus="$(lpq -a)"
-if test "$PrintStatus" != "no entries" ; then
-	jumpto CheckPrintStaus
-fi
-
-
-sleep 10
-
-./printcore.py -b 76800 -v '/dev/ttyS0' start2.gcode
-./printcore.py -b 76800 -v '/dev/ttyS0' download.gcode
-sleep 10
-
-./printcore.py -b 76800 -v '/dev/ttyS0' end.gcode
-
-
-#report Print Job Completed
-wget -O download.gcode "$printerServer?jobID=${PrintJobID:1}&stat=Done"
-
+source $PRINTERSTYLE-print.sh
 jumpto TheTop
--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
+#! /bin/bash
+
 screen -d -m /bin/bash './AutoDrop.sh'
 echo Process started
+screen -x
